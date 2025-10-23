e6c582fea7ecc2eb8bb2ff451fe4a893409a49df
RatonEMN <ajamais.ange@gmail.com>
2025-10-23 00:59:55 -0600

    Update 10/23 12:59 am

--- compras.js
+++ compras.js
@@ -1,1 +1,14 @@
 console.log("Mi programa de compras");
+//Calcule el total a pagar con IVA de una compra.
+
+let impuesto_IVA = 0.16;
+
+const prompt = require("prompt-sync")();
+
+const compra = prompt("Ingresa el total de la compra: ");
+        
+// Convierte la entrada a número decimal.
+const numberCompra = parseFloat(compra);
+let total_compra = numberCompra + (numberCompra*impuesto_IVA);
+
+console.log("El total de tu compra es: "+total_compra);
