d362a3badc95a0511c2bd9b7ec669d93af97673e
RatonEMN <ajamais.ange@gmail.com>
2025-10-23 11:49:10 -0600

    Se cambia la variable impuesto_IVA a const y se agregan instrucciones

--- compras_iva.js
+++ compras_iva.js
@@ -1,7 +1,9 @@
+//Calcule el total a pagar con IVA de una compra.
+
 console.log("Mi programa de compras");
 //Calcule el total a pagar con IVA de una compra.
 
-let impuesto_IVA = 0.16;
+const impuesto_IVA = 0.16;
 
 const prompt = require("prompt-sync")();
 
@@ -9,6 +11,6 @@ const compra = prompt("Ingresa el total de la compra: ");
         
 // Convierte la entrada a número decimal.
 const numberCompra = parseFloat(compra);
-let total_compra = numberCompra + (numberCompra*impuesto_IVA);
+let total_compra = numberCompra + (numberCompra*impuesto_IVA);  
 
 console.log("El total de tu compra es: "+total_compra);
