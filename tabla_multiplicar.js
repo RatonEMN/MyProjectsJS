19568bf673c37a687265306e32468fbd9639b5fc
RatonEMN <ajamais.ange@gmail.com>
2025-10-24 10:40:05 -0600

    Se validan si los numeros son correctos

--- tabla_multiplicar.js
+++ tabla_multiplicar.js
@@ -1,16 +1,24 @@
 //Genere una tabla de multiplicar del número que el usuario elija.
+console.log("Mi tabla de multiplicar");
 
 // Importa el módulo 'prompt-sync' que permite recibir entradas del usuario desde la terminal.
 
 const prompt = require("prompt-sync")();
 
 //Creo las variables para los numeros que me proporcionara el usuario:
-let dato1 = prompt("Ingresa un numero: ");
-let dato2 = prompt("Ingresa un segundo numero: ");
+const dato1 = prompt("Ingresa un numero: ");
+const dato2 = prompt("Ingresa un segundo numero: ");
 
 // Convierte la entrada a número decimal.
-let number1 = parseFloat(dato1);
-let number2 = parseFloat(dato2);
-let multiplicacion = number1*number2;
+const number1 = parseFloat(dato1);
+const number2 = parseFloat(dato2);
 
-console.log("El resultado de la mutiplicacion de los 2 numeros es: "+multiplicacion);
+
+if (isNaN(number1)&&isNaN(number2)) {
+  console.log("Por favor, ingresa numeros válidos.");
+} else {
+       
+let multiplicacion = number1*number2;
+console.log("El resultado de la mutiplicacion de los 2 numeros es: "+multiplicacion.toFixed(2));
+
+}
