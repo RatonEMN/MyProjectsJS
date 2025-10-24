0776e1ed61aa877a97014fde7e685cb0e616b969
RatonEMN <ajamais.ange@gmail.com>
2025-10-24 10:27:26 -0600

    Se agrego los decimales a mi resultado con tofixed y se validado que la entreda del usuario sea un numero

--- compras_iva.js
+++ compras_iva.js
@@ -8,9 +8,15 @@ const impuesto_IVA = 0.16;
 const prompt = require("prompt-sync")();
 
 const compra = prompt("Ingresa el total de la compra: ");
-        
+
 // Convierte la entrada a número decimal.
 const numberCompra = parseFloat(compra);
-let total_compra = numberCompra + (numberCompra*impuesto_IVA);  
 
-console.log("El total de tu compra es: "+total_compra);
+if (isNaN(numberCompra)) {
+  console.log("Por favor, ingresa un número válido.");
+} else {
+       
+let total_compra = numberCompra + (numberCompra*impuesto_IVA);  
+console.log("El total de tu compra es: "+total_compra.toFixed(2));
+
+}
