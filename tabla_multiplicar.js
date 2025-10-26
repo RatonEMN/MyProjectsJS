32cf01b128a6bc797ecbf7468b69c088c3ecbdbf
RatonEMN <ajamais.ange@gmail.com>
2025-10-26 00:47:39 -0600

    Se modifica el codigo, se realiza la tabla de multiplicar de acuerdo al numero que el usuario proporciona

--- tabla_multiplicar.js
+++ tabla_multiplicar.js
@@ -5,20 +5,24 @@ console.log("Mi tabla de multiplicar");
 
 const prompt = require("prompt-sync")();
 
-//Creo las variables para los numeros que me proporcionara el usuario:
-const dato1 = prompt("Ingresa un numero: ");
-const dato2 = prompt("Ingresa un segundo numero: ");
+function TablaMultiplicacion(){
+
+//Creo las variables para el numero que me proporcionara el usuario:
+const dato = prompt("Ingresa un numero: ");
 
 // Convierte la entrada a número decimal.
-const number1 = parseFloat(dato1);
-const number2 = parseFloat(dato2);
+const number = parseFloat(dato);
 
-
-if (isNaN(number1)&&isNaN(number2)) {
+if (isNaN(number)) {
   console.log("Por favor, ingresa numeros válidos.");
+  TablaMultiplicacion();
 } else {
-       
-let multiplicacion = number1*number2;
-console.log("El resultado de la mutiplicacion de los 2 numeros es: "+multiplicacion.toFixed(2));
+  for(let i = 1; i <= 10; i++){
+    let multiplicacion = number*i;
+    console.log(number+" x "+i+" = "+multiplicacion); 
+  }
 
 }
+}
+
+TablaMultiplicacion();
