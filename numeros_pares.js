720d3636c8367bbdde4b63d45afbf9567ca5f33e
RatonEMN <ajamais.ange@gmail.com>
2025-10-26 22:13:34 -0600

    Se agrega el programa de numeros pares

---
+++ numeros_pares.js
@@ -0,0 +1,16 @@
+//Cuente cuántos números pares hay entre 1 y 50
+
+const numero = 50;
+const divisor = 2;
+let numeros_pares = 0;
+
+//Para determinar si un numero es par o impar, se puede saber mediante el residuo de la operacion en este caso JS tiene la opcion de usar % para obtener el residuo de una operacion:
+
+for(let i = 1; i <= numero; i++){
+    if(i % 2 === 0){
+        numeros_pares++;
+    }
+}
+
+console.log("Los numeros pares son: "+numeros_pares);
+
