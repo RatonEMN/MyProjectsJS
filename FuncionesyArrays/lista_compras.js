64fad4ec53d51b6772a2cd9b739cefab07d80409
RatonEMN <ajamais.ange@gmail.com>
2025-11-01 01:32:06 -0600

    Inicio del codigo de lista compras

--- FuncionesyArrays/lista_compras.js
+++ FuncionesyArrays/lista_compras.js
@@ -1,1 +1,30 @@
 //Crea una lista de compras y agrega, elimina o busca productos.
+
+//Muestro al usuario un menu y le pido que ingrese una opcion
+
+const prompt = require("prompt-sync")();
+
+function menu(){
+
+console.log("-----Lista de compras-----");
+console.log("1. Agregar producto");
+console.log("2. Eliminar producto");
+console.log("3. Buscar producto");
+console.log("3. Ver lista completa");
+console.log("3. Salir...");
+
+const opcion = prompt("Ingresa una opcion: ");
+const noOpcion = parseInt(opcion);
+
+if (isNaN(noOpcion)) {
+  console.log("Por favor, ingresa un opcion válida.");
+  menu();
+} else {
+       
+console.log("Tu opcion es valida.");
+
+}
+
+}
+
+menu();
