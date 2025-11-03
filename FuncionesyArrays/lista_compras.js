f57ad604a45f18d9a828559480bbfe021fd058c6
RatonEMN <ajamais.ange@gmail.com>
2025-11-03 12:40:46 -0600

    Avance de la lista de compras, me quede en el metodo de eliminar un procuto

--- FuncionesyArrays/lista_compras.js
+++ FuncionesyArrays/lista_compras.js
@@ -4,27 +4,94 @@
 
 const prompt = require("prompt-sync")();
 
+
 function menu(){
+
+const productos = ["manzana","pera"];
 
 console.log("-----Lista de compras-----");
 console.log("1. Agregar producto");
 console.log("2. Eliminar producto");
 console.log("3. Buscar producto");
-console.log("3. Ver lista completa");
-console.log("3. Salir...");
+console.log("4. Ver lista completa");
+console.log("5. Salir...");
 
 const opcion = prompt("Ingresa una opcion: ");
 const noOpcion = parseInt(opcion);
 
-if (isNaN(noOpcion)) {
-  console.log("Por favor, ingresa un opcion válida.");
-  menu();
-} else {
-       
-console.log("Tu opcion es valida.");
+  switch(noOpcion){
+    case isNaN:
+    console.log("**********Por favor, ingresa un opcion válida.**********");
+    console.log("");
+    menu();
+    break;
+    case 1:
+    agregar_producto(productos);
+    break;
+    case 2:
+    eliminar_producto(productos);
+    break;
+    case 3:
+    console.log("Opcion 3");
+    break;  
+    case 4:
+    ver_productos(productos);
+    break;
+    case 5:
+    //Salir
+    break;
+    default:
+    console.log("**********Por favor, ingresa un opcion válida.**********");
+    console.log("");
+    menu();
+    break;
+  }
 
 }
 
+function agregar_producto(productos){
+console.log("");//Salto de linea
+const producto_nuevo = prompt("Ingresa el nombre del nuevo producto: ");
+if (producto_nuevo.length==0) {
+  console.log("Por favor, ingresa un producto.");
+  agregar_producto();
+} else {
+    productos.push(producto_nuevo);
+    console.log("✅Producto agregado");
+    console.log(""); //Salto de linea
+    menu();
+}
+}
+
+function eliminar_producto(productos){
+console.log("");//Salto de linea
+const producto_eliminar = prompt("Ingresa el nombre del producto: ");
+
+// Vamos a buscar la palabra y obtener su índice
+let indice = productos.indexOf(producto_eliminar); //REVISAR COMO FUNCIONA ESTE METODO
+console.log(`Índice encontrado: ${indice}`); // Muestra el indice: ejemplo: 1
+
+// Verificamos que el producto se encuentre en la lista
+if (indice !== -1) {
+    productos.splice(indice, 1);
+}else{
+    console.log("El producto que ingresaste no existe, intentalo nuevamente");
+    eliminar_producto();
+}
+
+console.log(productos); 
+
+}
+
+function ver_productos(productos){
+    console.log("");//Salto de linea
+    console.log("🔹Tus productos son los siguientes:");
+    for(let i = 0; i <= productos.length-1; i++){
+        console.log(`${i+1}.-${productos[i]}`);
+    }
+    const opcion_menu = prompt("----Para regresar al menu, presione enter.----");
+    console.log("");//Salto de linea
+    menu();
 }
 
 menu();
