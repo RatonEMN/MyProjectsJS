5f298b53cf281dbef08e7c3de58acb395ae5c11a
RatonEMN <ajamais.ange@gmail.com>
2025-11-10 10:58:07 -0600

    Se agregan comentarios

---
+++ ObjetosyDatos/objeto_carro.js
@@ -0,0 +1,17 @@
+//Crea un objeto que represente un auto con propiedades (marca, modelo, año, color).
+
+//Creo mi obejeto, esta es mi sintaxis
+const carro = {
+    marca: "honda",
+    modelo: "civic",
+    year: 2007,
+    color: "gris plata"
+};
+
+//Mando a imprimir mis resultados
+console.log("-----Este es mi obejeto carro----");
+console.log(`Marca: ${carro.marca}`);
+console.log(`Modelo: ${carro.modelo}`);
+console.log(`Año: ${carro.year}`);
+console.log(`color: ${carro.color}`);
+console.log("--------------------------------");
