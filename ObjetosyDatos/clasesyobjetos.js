c0fadb40b9d5fa5cf48c3051167e7751d11854b1
RatonEMN <ajamais.ange@gmail.com>
2025-11-10 11:03:23 -0600

    Se agregan comentarios

--- ObjetosyDatos/clasesyobjetos.js
+++ ObjetosyDatos/clasesyobjetos.js
@@ -1,6 +1,7 @@
 //Crea una clase Auto y permite crear varias instancias
 
-class auto {
+//Creo mi clase(plantilla) con el constructor, esta es mi sintaxis: 
+class Auto {
     constructor(marca,modelo,year,color){
         this.marca = marca;
         this.modelo = modelo;
@@ -8,15 +9,18 @@ class auto {
         this.color = color;
     }
 
-obtenerInfo(){
+obtenerInfo(){ //Dentro de mi clase creo una funcion, la cual usare mas tarde para imprimir los datos
     return(`El auto es un ${this.marca} modelo ${this.modelo} del año ${this.year} y de color ${this.color}`);
 }
 }
 
-const auto_toyota = new auto("Toyota","Tacoma",2025,"negro");
-const auto_honda = new auto("Honda","Civic",2007,"gris plata");
-const auto_ford = new auto("Ford","Ranger",2002,"blanco");
+//Creo mis objetos, en la cual mando mis argumentos(propiedades) a la clase auto
+const auto_toyota = new Auto("Toyota","Tacoma",2025,"negro");
+const auto_honda = new Auto("Honda","Civic",2007,"gris plata");
+const auto_ford = new Auto("Ford","Ranger",2002,"blanco");
 
+
+//Aqui mando a imprimir los datos de mis objetos invocando la funcion que tengo en mi clase auto
 console.log("------------");
 console.log(auto_toyota.obtenerInfo());
 
