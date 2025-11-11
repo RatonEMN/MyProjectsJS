037fa5603be274443c05c83615dbe25aa8bcc33f
RatonEMN <ajamais.ange@gmail.com>
2025-11-11 00:41:46 -0600

    Se agregan comentarios y codigo

--- ObjetosyDatos/recorre_objetos.js
+++ ObjetosyDatos/recorre_objetos.js
@@ -1,2 +1,26 @@
 //Crea una función que recorra un arreglo de objetos y muestre solo los autos más recientes.
 
+const autos = [
+    {
+        marca: "Toyota",
+        modelo: "Tacoma",
+        year: 2016
+    },
+    {
+        marca: "Honda",
+        modelo: "Civic",
+        year: 2010
+    },
+    {
+        marca: "Nissan",
+        modelo: "Versa",
+        year: 2016
+    },
+    {
+        marca: "Ford",
+        modelo: "Ranger",
+        year: 2012
+    }
+];
+
+//existen 2 formas de recorrer mi arreglo con filter y con foreach, tambien se pude hacer con un bucle for u of
