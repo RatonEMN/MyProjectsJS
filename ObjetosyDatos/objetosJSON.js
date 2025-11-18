5df16492225a0390704673e426f38657a515feca
RatonEMN <ajamais.ange@gmail.com>
2025-11-18 12:10:05 -0600

    Se actualizar archivo final objetosJSON.js

--- ObjetosyDatos/objetosJSON.js
+++ ObjetosyDatos/objetosJSON.js
@@ -1,23 +1,53 @@
 //Convierte el objeto a JSON y viceversa.
 
-const carro = {
-    marca: "honda",
-    modelo: "civic",
-    year: 2007,
-    color: "gris plata"
+
+//Los archivos JSON se refieren al formato, el cual es importante para poder mandar la informacion a servidores, cosas importantes a considerar:
+//No se pueden convertir funciones a JSON, tampoco valores undefined o valores tipo Symbol.
+
+
+//Creo mi objeto llamado tareaPendiente:
+const tareaPendiente = {
+  // String (La descripción de la tarea)
+  descripcion: "Enviar reporte trimestral de ventas", 
+  
+  // Booleano (Estado de la tarea)
+  completada: false,    
+  
+  // Número (ID de la tarea)
+  id: 42,      
+  
+  // Array (Personas responsables)
+  responsables: ["Ana", "Pedro"], 
+  
+  // Undefined (Un valor que no debe aparecer en el resultado JSON)
+  prioridad: undefined, 
+  
+  // Función (Una función que NO debe aparecer en el resultado JSON)
+  mostrarEstado: function() {
+    console.log(`Estado: ${this.completada ? 'Terminada' : 'Pendiente'}`);
+  } 
 };
 
 
-//Convertir mi objeto carro de JS a objeto JSON:
-const objJSON = JSON.stringify(carro);
-console.log(objJSON);
+//Convierto mi objeto a JSON:
+console.log("Objeto en formato JSON:");
+const tareaPendienteJSON = JSON.stringify(tareaPendiente);
 
-//Viceversa; Convertir mi objeto JSON a objeto JS:
-const objJS = JSON.parse(objJSON);
-console.log(objJS.marca);
+//Se pueden pasar parametros adicionales al momento de serializar mi obejto los cuales son replacer y spacer,
+//replacer: Me ayuda a filtrar datos, modificarlos
+//spacer: Me ayuda a identar los datos para que sean mas legibles
+
+//const tareaPendienteJSON = JSON.stringify(tareaPendiente,null,2); 
+
+console.log(tareaPendienteJSON);
 
 
-//ME QUEDE EN REVISAR LAS CARACTERISTICAS PARA LA CONVERSION DE OBJETO A JSON, y hacer un nuevo ejercicio
+console.log("-------------------------------------------------");
 
 
+console.log("Objeto en formato JS:");
+const tareaPendienteJS = JSON.parse(tareaPendienteJSON);
+console.log(tareaPendienteJS);
 
+console.log("Propiedades después de convertir a JSON:");
+console.log(Object.keys(tareaPendienteJS));
