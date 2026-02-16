f6a3289116c94c72be75409bf9364e5885948d00
RatonEMN <ajamais.ange@gmail.com>
2026-02-16 00:24:16 -0600

    Se agrega el archivo .gitignore para excluir las dependecias de node de manera profesional

---
+++ WK4 Dom y eventos/contador/app.js
@@ -0,0 +1,13 @@
+/*console.log(window);
+console.log(document);
+
+let texto = "Hola esta es un prueba";
+
+const hablar =(texto)=>
+    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+hablar(texto);
+*/ // Este codigo hace que tu computadora hable
+
+
+const elementoContador = document.getElementById('valor-contador');
+elementoContador.textContent = 'Nuevo Valor';
