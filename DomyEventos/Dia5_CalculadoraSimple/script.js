d4122164924020d2f3df7d1b2c5a6a39985f9ea5
Emanuel <ajamais.ange@gmail.com>
2026-02-27 11:09:14 -0600

    Se inica practica Dia5, se crea la carpeta Dia_CalculadoraSimple y se crean archivos iniciales

---
+++ DomyEventos/Dia5_CalculadoraSimple/script.js
@@ -0,0 +1,9 @@
+//1. Seleccionar DOM
+//2. Crear funciones matemáticas
+//3. Crear función obtenerValores()
+//4. Crear función mostrarResultado()
+//5. Agregar event listeners
+//6. Crear función limpiar()
+// Y la validación:
+//  - Números → en obtenerValores()
+//  - División entre cero → dentro de dividir()
