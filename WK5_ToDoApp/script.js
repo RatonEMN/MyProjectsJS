40b493d55e1729cdb54f92b02e7e0c08b1716a70
RatonEMN <ajamais.ange@gmail.com>
2026-03-31 12:27:52 -0600

    Se inicia la semana 5 con el proyecto de To-DoApp

---
+++ WK5_ToDoApp/script.js
@@ -0,0 +1,29 @@
+//1. Crea el html
+//2. Seleccionar el DOM
+//3. Crear el array en donde se guardaran los datos
+//4. Cargar datos localmente
+//5. Funcion de render
+//6. Eventos: input, boton, clicks
+//7. Funciones de logica CRUD
+//8. Guardar datos en local storage
+
+const inputTask = document.querySelector("#inputTask");
+const btnSave = document.querySelector("#save");
+
+const lisTask = document.querySelector("#tasks");
+
+    //Este es mi array en donde guardare mis tareas
+const tareas = ["Lavar los trastes","Lavar la ropa"];
+
+btnSave.addEventListener("click",function(){
+    //Leemos el valor del inputTask
+    const nuevaTarea = inputTask.value;
+    
+    tareas.push(nuevaTarea);
+
+    console.log(tareas);
+
+    //Limpiamos el input
+    inputTask.value = "";
+
+});
