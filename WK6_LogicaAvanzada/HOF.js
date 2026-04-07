8c85a63c2a2bdb921e91f235e80b2924d8c6d2af
RatonEMN <ajamais.ange@gmail.com>
2026-04-07 00:34:35 -0600

    Se termina la practida del dia 1 HOF

--- WK6_LogicaAvanzada/HOF.js
+++ WK6_LogicaAvanzada/HOF.js
@@ -26,3 +26,46 @@ const productosCaros = productos.filter(function(objeto){
 });
 
 console.log(productosCaros);
+
+
+// 🧩 Parte 3: reduce
+
+// 👉 Calcula el precio total de todos los productos
+
+const precioTotal = productos.reduce(function(acumulador, actual) {
+  return acumulador + actual.precio;
+}, 0);
+
+console.log("El precio total es: "+precioTotal);
+
+
+// 🧩 Parte 4: tu propio map
+
+// 👉 Crea tu propia versión de map llamada:
+
+const numeros = [2, 4, 6, 8];
+
+function miMap(arreglo,callback) {
+  // crear nuevo arreglo
+  const nuevoArreglo = [];
+  // recorrer arreglo
+  for(let i = 0; i <= arreglo.length-1; i++){
+    // aplicar callback
+    let resultado = callback(arreglo[i]);
+    // guardar resultado
+    nuevoArreglo.push(resultado);
+  }
+
+  return nuevoArreglo;
+
+  // retornar nuevo arreglo
+}
+
+function callback(numero){
+    return numero * 5;
+}
+
+
+const nuevoArray = miMap(numeros,callback);
+
+console.log(nuevoArray);
