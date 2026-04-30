0898f25715e994fb01ed31af1d1eafae2356bc03
Emanuel <ajamais.ange@gmail.com>
2026-04-30 12:17:30 -0600

    Se termina el ejemplo de callback hell

--- WK6_LogicaAvanzada/CallbackHell.js
+++ WK6_LogicaAvanzada/CallbackHell.js
@@ -54,3 +54,90 @@ pedirNombre(function (nombre) {
   });
 
 });
+
+
+
+// paso1(function () {
+//   paso2(function () {
+//     console.log("Fin");
+//   });
+// });
+
+
+
+
+
+// function despuesDePaso2() {
+//   console.log("Fin");
+// }
+
+// function despuesDePaso1() {
+//   paso2(despuesDePaso2);
+// }
+
+// paso1(despuesDePaso1);
+
+
+
+
+
+function paso1(callback) {
+  setTimeout(function () {
+    console.log("Paso 1 listo");
+    callback();
+  }, 1000);
+}
+
+function paso2(callback) {
+  setTimeout(function () {
+    console.log("Paso 2 listo");
+    callback();
+  }, 1000);
+}
+
+function paso3(callback) {
+  setTimeout(function () {
+    console.log("Paso 3 listo");
+    callback();
+  }, 1000);
+}
+
+
+// // ❌ Callback hell (anónimo)
+// paso1(function () {
+
+//   paso2(function () {
+
+//     paso3(function () {
+
+//       console.log("Proceso terminado");
+
+//     });
+
+//   });
+
+// });
+
+
+
+
+// 1️⃣ función para el último paso
+function despuesDePaso3() {
+  console.log("Proceso terminado");
+
+}
+
+// 2️⃣ función después de paso2
+function despuesDePaso2() {
+  paso3(despuesDePaso3);
+     
+}
+
+// 3️⃣ función después de paso1
+function despuesDePaso1() {
+  paso2(despuesDePaso2);
+
+}
+
+// 4️⃣ iniciar proceso
+paso1(despuesDePaso1);
